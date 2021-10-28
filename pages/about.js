073a02c404ec9dbdbbdd6e41f8e2c25c46a651a4
DaNialz <danialz804@gmail.com>
2021-10-28 16:41:44 +0330

    routing and navigation

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default function About() {
+    return (<div>
+        <Link href="/"><a>Home</a></Link>
+        <h1>This is the About us page</h1>
+    </div>)
+} 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
+import Link from "next/link";
+
 export default function Index() {
     return (
         <div>
+            <Link href="/about"><a>About</a></Link>
             <h1>Hello from Index Page</h1>
         </div>
     )
